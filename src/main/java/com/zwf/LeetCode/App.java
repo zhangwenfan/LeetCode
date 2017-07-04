8f9f78fc994d03ed827097c219bee02cdd34a583
zwf <zhangwenfancgm@gmail.com>
2017-07-04 17:28:42 +0200

    update

--- src/main/java/com/zwf/LeetCode/App.java
+++ src/main/java/com/zwf/LeetCode/App.java
@@ -1,13 +1,15 @@
 package com.zwf.LeetCode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		Map<Integer, Integer> map = new HashMap<>();
+	}
 }
