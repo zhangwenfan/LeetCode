b6cbd28ebdd8644466809f51b3ec83bb7f0c3ac8
zwf <wenfan.zwf@alibaba-inc.com>
2017-09-08 16:19:52 +0800

    test add

---
+++ src/main/java/com/zwf/testlombok/Test.java
@@ -0,0 +1,24 @@
+/**
+ * LeetCode Test.java
+ * 
+ * File Created at 2017年9月8日 上午11:21:05
+ * $Id$
+ * 
+ * Copyright 2013 Taobao.com Croporation Limited.
+ * All rights reserved.
+ */
+package com.zwf.testlombok;
+
+/**
+ * @create 2017年9月8日 上午11:21:05
+ * @author zwf
+ * @version
+ */
+public class Test {
+    public static void main(String[] args) {
+        User u = new User();
+        System.out.println(u.getName() + "1");
+        
+    }
+
+}
---
+++ src/main/java/com/zwf/testlombok/User.java
@@ -0,0 +1,25 @@
+/**
+ * LeetCode User.java
+ * 
+ * File Created at 2017年9月8日 上午11:19:14
+ * $Id$
+ * 
+ * Copyright 2013 Taobao.com Croporation Limited.
+ * All rights reserved.
+ */
+package com.zwf.testlombok;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @create 2017年9月8日 上午11:19:14
+ * @author zwf
+ * @version
+ */
+@Setter
+@Getter
+public class User {
+    String name = "zwf";
+}
